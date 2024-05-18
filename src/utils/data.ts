eb06b9d6556c4aa15f84b0fdc428ff34e582fd64
vinothvkr <vinothvkr@hotmail.com>
2024-05-18 13:15:07 +0530

    add post type

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -4,14 +4,42 @@ import matter from 'gray-matter';
 
 const dir = path.join(process.cwd(), '_posts');
 
+export type Post = {
+  id: number;
+  slug: string;
+  published: boolean;
+  title: string;
+  date: string;
+  image: string;
+  category: string;
+  tags: string[];
+  excerpt?: string;
+  content: string;
+};
+
 export const getAllBlogPosts = () => {
   const allFiles = fs.readdirSync(dir);
   const allBlogs = allFiles.map(file => {
     const filePath = path.join(dir, file);
     const fileContent = fs.readFileSync(filePath, 'utf-8');
-    const { data, content, excerpt } = matter(fileContent, { excerpt: true });
-    return { data, content, excerpt };
+    const { data, content, excerpt } = matter(fileContent, {
+      excerpt: true,
+    });
+    const post: Post = {
+      id: data.id,
+      slug: data.slug,
+      published: data.published,
+      title: data.title,
+      date: data.date,
+      image: data.image,
+      category: data.category,
+      tags: data.tags,
+      excerpt: excerpt,
+      content: content,
+    };
+    debugger;
+    return post;
   });
 
-  return allBlogs.filter(x => x.data.published === true);
+  return allBlogs.filter(x => x.published === true);
 };
